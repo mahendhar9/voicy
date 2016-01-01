978ebe696d81f455f6602f57b8aaaae7ed5ba563
Mahendhar <mahendarreddy@live.com>
2016-01-02 02:27:13 +0530

    User can now search Pixabay images via voice

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('voicy', ['ui.router'])
+angular.module('voicy', ['ui.router', 'ngResource'])
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise("/");
   $stateProvider
@@ -10,15 +10,19 @@ angular.module('voicy', ['ui.router'])
   })
 }])
 
-.controller('HomeCtrl', function() {
+.controller('HomeCtrl', function($resource) {
   var home = this;
+
+  home.pixabayApi = $resource("https://pixabay.com/api/?key=1835217-f660445e7f88a77e646ed233", {callback: "JSON_CALLBACK"},{get: {method: "JSONP"}});
+
   if (annyang) {
     var commands = {
-      'say hello': function() {
-        alert("Hello Mahendhar");
+      'search *term': function(term) {
+        home.showTerm = "Search " + term;
+        console.log("Search " + term);
+        home.showPhotos = home.pixabayApi.get({q: term, image_type: 'photo'});
       }
     };
-
     annyang.addCommands(commands);
 
     annyang.start();
